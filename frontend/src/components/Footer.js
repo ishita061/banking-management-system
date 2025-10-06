f3e997ad569ceccb4ca22ab8df35e01f93b582ba
Ishita chaturvedi <168914437+ishita061@users.noreply.github.com>
2025-10-06 16:58:39 +0530

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer style={{ background: "#002171", color: "white", textAlign: "center", padding: "12px", marginTop: "20px" }}>
+      © 2025 Banking Management System
+    </footer>
+  );
+}
