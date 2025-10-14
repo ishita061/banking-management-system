ee755cfbaa2d5c714ef10098b3097946e8406e33
Ishita chaturvedi <168914437+ishita061@users.noreply.github.com>
2025-10-14 10:22:53 +0530

    Create db.js

---
+++ backend/routes/db.js
@@ -0,0 +1,18 @@
+import mysql from "mysql2";
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",          
+  password: "",          
+  database: "bank_db"    
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("Connected to MySQL database");
+  }
+});
+
+export default db;
